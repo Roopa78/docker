20b8c79c298a4a91dd75215ea6b455a98647d62a
Roopa78 <roopapj150@gmail.com>
2025-12-02 17:47:23 +0530

    raect-js app files created

---
+++ react-app/src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "40px",
+      fontWeight: "bold"
+    }}>
+      Hello from React App! 🚀
+    </div>
+  );
+}
+
+export default App;
